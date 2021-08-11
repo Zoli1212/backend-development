ed5de99a79be707da6730be20468de1a8966a51e
Your Name <you@example.com>
2021-08-11 10:04:39 +0200

    first commit

---
+++ routes/orders.js
@@ -0,0 +1,49 @@
+const {Order} = require('../models/order');
+const express = require('express');
+const { OrderItem } = require('../models/order-item');
+const router = express.Router();
+
+router.get(`/`, async (req, res) =>{
+    const orderList = await Order.find();
+
+    if(!orderList) {
+        res.status(500).json({success: false})
+    } 
+    res.send(orderList);
+})
+
+router.post('/', async(req, res) =>{
+
+    const orderItemsId = req.body.orderItems.map(async (orderitem) => {
+        let newOrderItem = new OrderItem({
+            quantity: orderItem.quantity,
+            product: orderItem.product
+        })
+
+        newOrderItem = await newOrderItem.save()
+
+        return newOrderItem._id
+
+    } )
+    let order = new Order({
+        orderItems: orderItemsId,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: totalPrice,
+        user: req.body.user,
+    })
+   // order = await category.save();
+
+    if(!order)
+    return res.status(404).send('the order cannot be created!')
+
+    res.send(order);
+
+})
+
+module.exports =router;
